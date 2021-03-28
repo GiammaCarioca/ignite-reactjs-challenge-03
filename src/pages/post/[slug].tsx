1c1575ec3651d5583734a76ccad46acd5e3e4e44
Thiago Giammattey <giammattey@gmail.com>
2021-03-28 12:48:08 -0300

    style page post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +29,49 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Header />
+      <img
+        src="/banner.png"
+        alt="Eat Sleep Code Repeat"
+        className={styles.banner}
+      />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Título</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                28 Mar 2021
+              </li>
+              <li>
+                <FiUser />
+                Thiago Giammattey
+              </li>
+              <li>
+                <FiClock />5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Título seção</h2>
+            <p>
+              Lorem, ipsum <a href="/">dolor</a> sit amet consectetur
+              adipisicing elit. Sed debitis eligendi eos nihil! Facilis
+              molestiae doloribus minus quia similique consectetur{' '}
+              <strong>inventore</strong> quisquam cumque, dignissimos, debitis
+              placeat? Reprehenderit repudiandae in suscipit?
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
